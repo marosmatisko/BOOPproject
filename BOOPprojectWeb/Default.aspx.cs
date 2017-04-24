e900197de1129a71f77bbd138e6d4b96e6628c29
Ondrej Gajdusek <ogajduse@redhat.com>
2017-04-24 17:46:24 +0200

    fix formatting

--- BOOPprojectWeb/Default.aspx.cs
+++ BOOPprojectWeb/Default.aspx.cs
@@ -8,13 +8,10 @@ using System.Web.UI.WebControls;
 using BOOPproject;
 
 namespace BOOPprojectWeb {
-    public partial class Default : System.Web.UI.Page
-    {
-        protected void Page_Load(object sender, EventArgs e)
-        {}
+    public partial class Default : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) { }
 
-        protected void Confirm_btn_Click(object sender, EventArgs e)
-        {
+        protected void Confirm_btn_Click(object sender, EventArgs e) {
             StringStatistics statistics = new StringStatistics(TextBox1.Text);
 
             ResultsTable.Rows[0].Cells[1].Text = Convert.ToString(statistics.GetSentencesNumber());
@@ -38,11 +35,9 @@ namespace BOOPprojectWeb {
             PrintDicionary(statistics.CharMap, statistics.GetCharacterCount(), CharMapDiv);
         }
 
-        private void PrintDicionary<T>(Dictionary<T, int> dic, int count, HtmlGenericControl div)
-        {
+        private void PrintDicionary<T>(Dictionary<T, int> dic, int count, HtmlGenericControl div) {
             div.InnerHtml = "";
-            foreach (KeyValuePair<T, int> entry in dic.OrderByDescending(key => key.Value).ThenBy(key => key.Key))
-            {
+            foreach (KeyValuePair<T, int> entry in dic.OrderByDescending(key => key.Value).ThenBy(key => key.Key)) {
                 div.InnerHtml +=
                     $"<a href=\"#\" data-toggle=\"tooltip\" title=\"{entry.Value}x&nbsp;&nbsp;{Math.Round(Convert.ToDouble(entry.Value) / count * 100, 2)}%\">{entry.Key}</a> | ";
             }
